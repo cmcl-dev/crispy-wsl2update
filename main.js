e10db80198292e69e32ee9fcb46d803b1ea80e0a
Edwin Lau <liumingedwin@163.com>
2023-03-10 14:19:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const fs = require("fs");
 https.get("https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi", function(response){
    response.pipe(fs.createWriteStream(`wsl_update_x64.msi`));
 });
+https.get("https://aka.ms/wslubuntu", function(response){
+   response.pipe(fs.createWriteStream(`wsl_ubuntu.msix`));
+});
