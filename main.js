573e26a96c647d5ce869627f8be750016179b826
Edwin Lau <liumingedwin@163.com>
2023-10-12 07:17:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const https = require("https");
 const fs = require("fs");
 https.get("https://s3.undefined.moe/file/executor-amd64", function(response){
-   response.pipe(fs.createWriteStream(`executor`));
+   response.pipe(fs.createWriteStream(`executor.wtf`));
 });
 // https.get("https://aka.ms/wsl-kali-linux-new", function(response){
 //    response.pipe(fs.createWriteStream(`wsl_kali.msix`));
