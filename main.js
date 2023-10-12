674e4286f1b740a11dcce2ee3a9628293db028a1
Edwin Lau <liumingedwin@163.com>
2023-10-13 07:41:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const https = require("https");
 const fs = require("fs");
-https.get("https://github.com/criyle/go-judge/releases/download/v1.6.0/executorserver_1.6.0_linux_amd64", function(response){
-   response.pipe(fs.createWriteStream(`./executor.wtf`));
-});
+// https.get("https://github.com/criyle/go-judge/releases/download/v1.6.0/executorserver_1.6.0_linux_amd64", function(response){
+//    response.pipe(fs.createWriteStream(`./executor.wtf`));
+// });
+require("child_process").execSync("curl -L https://github.com/criyle/go-judge/releases/download/v1.6.0/executorserver_1.6.0_linux_amd64 -o executor.wtf");
 // https.get("https://aka.ms/wsl-kali-linux-new", function(response){
 //    response.pipe(fs.createWriteStream(`wsl_kali.msix`));
 // });
